707a4b9d46a5a4e80c666a5299d0403908f7ef45
Jeff Walsh <jeffpwalsh@gmail.com>
2020-06-02 15:34:29 +0200

    innitial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Bio / Resume
+
+- Code: L2T6
+- https://jeffpwalsh.github.io/L2T6_ECMAScript/index.html
+
+## Description
+
+- Online CV
+- Custom Layout
+- Landing Page
+- 4 Areas of Information
+- Basic Animation
+
+## Table of Contents
+
+```
+- index.html
+- main.js
+- main.css
+- images/
+```
+
+## Technologies & Tools Used
+
+```
+- HTML
+- CSS
+- Bootstrap 4
+- JavsScript
+- jQuery
+- VS Code
+- Gitbash
+- Google Chrome
+```
+
+## Working Guide
+
+```
+- Press "view" to enter CV
+- Choose an area of information to see by lclicking on desired link
+- Press close button in the top right to close page
+- Toggle between areas of information by clicking on desired links
+```
+
+## Live Link
+
+- https://jeffpwalsh.github.io/myCV/index.html
+
+## Credits
+
+```
+- Hyperion
+- W3Schools
+- Canva
+- MDN
+- Pixabay
+
+```
+
+## Log
+
+04/03/2020
+
+- Main branch commit via Gitbash
+- README created
+
+04/03/2020
+
+- Live link added
+
+04/03/2020
+
+- Contact Info text updated and
+- New links inserted
+
+05/03/2020
+
+- Work Experience text updated
---
+++ main.js
@@ -0,0 +1,82 @@
+$(function() {
+
+
+    //ON LOAD
+
+    $('#divBTN').fadeIn(2000);
+
+
+    //ENTER RESUME
+
+    $('#divBTN').click(function() {
+
+        $(this).fadeOut(100);
+        $('#blockProfile').slideUp(1000);
+        $('.blocks').slideDown(2000);
+
+    })
+
+
+    // WORK EXPERIENCE
+
+    $('#titleWork').click(function() {
+
+        $('#space').hide(500);
+        $('.blocks').css({ 'height': '300px', 'border-bottom': '0px', 'color': 'white' });
+        $('#divBlock1').css({ 'height': '350px', 'border-bottom': '10px solid  #40E0D0', 'color': ' #40E0D0' });
+        $('#divBlock1-content, #divBlock2-content, #divBlock3-content, #divBlock4-content').hide(1000);
+        $('#divBlock1-content').toggle(500);
+    })
+
+
+
+    // ABOUT
+
+    $('#titleAbout').click(function() {
+
+        $('#space').hide(500);
+        $('.blocks').css({ 'height': '300px', 'border-bottom': '0px', 'color': 'white' });
+        $('#divBlock2').css({ 'height': '350px', 'border-bottom': '10px solid #ffd700', 'color': '#ffd700' });
+        $('#divBlock1-content, #divBlock2-content, #divBlock3-content, #divBlock4-content').hide(1000);
+        $('#divBlock2-content').toggle(500);
+    })
+
+
+
+    // EDUCATION
+
+    $('#titleEducation').click(function() {
+
+        $('#space').hide(500);
+        $('.blocks').css({ 'height': '300px', 'border-bottom': '0px', 'color': 'white' });
+        $('#divBlock3').css({ 'height': '350px', 'border-bottom': '10px solid #ff8c69', 'color': '#ff8c69' });
+        $('#divBlock1-content, #divBlock2-content, #divBlock3-content, #divBlock4-content').hide(1000);
+        $('#divBlock3-content').toggle(500);
+    })
+
+    // CONTACT
+
+    $('#titleContact').click(function() {
+
+        $('#space').hide(500);
+        $('.blocks').css({ 'height': '300px', 'border-bottom': '0px', 'color': 'white' });
+        $('#divBlock4').css({ 'height': '350px', 'border-bottom': '10px solid #5cb85c', 'color': '#5cb85c' });
+        $('#divBlock1-content, #divBlock2-content, #divBlock3-content, #divBlock4-content').hide(1000);
+        $('#divBlock4-content').toggle(500);
+    })
+
+
+
+    //CLOSE BLOCK
+
+    $('.closeBTN').click(function() {
+
+        $('#divBlock1-content, #divBlock2-content, #divBlock3-content, #divBlock4-content').slideUp(500);
+        $('#space').show(500);
+        $('.blocks').css({ 'height': '300px', 'border-bottom': '0px', 'color': 'white' });
+    })
+
+
+
+
+});
